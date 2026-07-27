ad592f10b441eb252e78ec541e141b7a4ac779ab
Hafsa Rahim <hafsa.rahim@gmail.com>
2026-07-27 13:43:12 +0500

    Fix production login API URL

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -8,7 +8,7 @@ export default function SignIn() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:3001/api/login", {
+      const response = await fetch("/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
